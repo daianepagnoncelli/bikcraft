cc68b441b483fce91aaffbb39f3737be3c526065
DaianePagnoncelli <d_pagnoncelli@hotmail.com>
2023-03-29 12:22:11 +0200

    javascript 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log("teste");
+// Ativar links do menu
+
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    console.log(link);
+  }
+}
+links.forEach(ativarLink);
+
+// Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+
+  if (elemento) {
+    elemento.checked = true;
+    console.log(elemento);
+  }
+}
+parametros.forEach(ativarProduto);
